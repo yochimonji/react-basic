4e37a654474fb1e15051e71932eb6759ebf6f83e
yochimonji <yochimonji@gmail.com>
2021-09-05 00:49:57 +0900

    add 9回完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import { Article, Counter } from "./components/index"
+import { useEffect, useState } from "react";
 
 function App() {
+  const [name, setName] = useState('');
+  const [id, setId] = useState('yochimonji');
+  const ids = ['yochimonji', 'deatiger', 'gaearon', 'aws', 'google', 'facebook'];
+  const gettRandomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)];
+    setId(_id);
+  }
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setName(data.name);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }, [id]);
+
   return (
     <div>
-      <Article
-        title={'新・日本一わかりやすいReact入門4'}
-        content={'今日のトピックはpropsについて。'}
-      />
-      <Article
-        title={'新・日本一わかりやすいReact入門5'}
-        content={'今日のトピックはuseStateについて。'}
-      />
-      <Article
-        title={'新・日本一わかりやすいReact入門6'}
-        content={'今日のトピックはuseEffectについて。'}
-      />
-      <Counter></Counter>
+      <p>{id}の、GitHub上の名前は{name}です。</p>
+      <button onClick={gettRandomId}>IDを変更</button>
     </div>
   );
 }
